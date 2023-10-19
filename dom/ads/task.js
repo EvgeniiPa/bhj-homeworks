485a4f4d6bca905b62fdf24e03ac789caf6146bc
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-10-19 17:08:23 +0300

    Homework 19.10

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,22 +1,22 @@
 let rotatorCase = document.querySelectorAll(".rotator__case");
 let rotator = document.querySelector(".rotator");
 let arr = Array.from(rotatorCase);
-let sum = 0;
+
+let sum = 1;
 
 let id = setInterval(() => {
   if (sum == 0) {
     arr[5].classList.remove("rotator__case_active");
     arr[sum].classList.add("rotator__case_active");
   }
-  arr[sum].classList.remove("rotator__case_active");
-  arr[sum + 1].classList.add("rotator__case_active");
-  console.log(arr[sum]);
-  sum++;
 
-  if (sum == 5) {
+  if (sum > 0) {
+    arr[sum - 1].classList.remove("rotator__case_active");
     arr[sum].classList.add("rotator__case_active");
-    sum = 0;
   }
+
+  sum++;
+  sum = sum % arr.length;
 }, 1000);
 
 // rotator__case_active
