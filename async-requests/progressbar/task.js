b4955c34779077153126205c54b742ab5faa7f0f
Evgenii Pasechnikov <evgenii.pase4nickov@gmail.com>
2023-11-21 14:18:17 +0300

    Homework 21.11

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,4 +1,6 @@
 let progress = document.getElementById("progress");
+progress.value = 0.0;
+sum = 0;
 let btn = document.querySelector("#send");
 
 let xhr = new XMLHttpRequest();
@@ -8,25 +10,23 @@ let form = document.querySelector("#form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  xhr.addEventListener("readystatechange", (e) => {
-    progress.value = 0.0;
-  });
-
   let formData = new FormData(form);
 
   xhr.open("POST", url);
 
-  xhr.upload.onprogress = function () {
-    progress.value = 0.5;
+  xhr.upload.addEventListener("progress", (e) => {
+    if (e.loaded < e.total) {
+      let valueProgress = e.loaded / e.total;
+      progress.value = valueProgress;
+    } else if (e.loaded == e.total) {
+      progress.value = 1;
+    }
+  });
+
+  xhr.upload.onerror = (e) => {
+    console.log(`Произошла ошибка во время отправки: ${xhr.status}`);
   };
 
-  xhr.upload.onload = function () {
-    progress.value = 1;
-  };
-
-  xhr.upload.onerror = function () {
-    alert("ошибка");
-  };
-
+  xhr.open("POST", url);
   xhr.send(formData);
 });
